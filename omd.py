ecb5c44ce7f6589d998853a4f17895d062c81229
DiGregory <cheburator20087@rambler.ru>
2022-09-13 02:45:49 +0300

    git commit

---
+++ omd.py
@@ -0,0 +1,24 @@
+def step2_umbrella():
+    print('Утку унесло на зонте ураганом')
+
+def step2_no_umbrella():
+    print('Утку смыло дождём')
+
+def step1():
+    print(
+        'Утка-маляр 🦆 решила выпить зайти в бар. '
+        'Взять ей зонтик? ☂️'
+    )
+    option = ''
+    options = {'да': True, 'нет': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+
+    if options[option]:
+        return step2_umbrella()
+    return step2_no_umbrella()
+
+
+if __name__ == '__main__':
+    step1()
